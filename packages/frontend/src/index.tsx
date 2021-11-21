0542b52220d7a91379219ab876586521206bc06b
Jatin Agrawal <agrawaljatin22@gmail.com>
2021-11-21 23:25:56 +0530

    setup: FE PWA manifest and service worker

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,6 +15,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
