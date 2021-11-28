c0ac7b7d2e822b92db260ab47f531a4e0d7f5800
Jatin Agrawal <agrawaljatin22@gmail.com>
2021-11-28 17:48:36 +0530

    fix: FE menu drawer scrollbar

--- packages/frontend/src/components/NavBar/MenuDrawer.tsx
+++ packages/frontend/src/components/NavBar/MenuDrawer.tsx
@@ -1,7 +1,6 @@
-import {
-  SwipeableDrawer, Drawer, Toolbar, useMediaQuery,
-} from '@mui/material';
+import { SwipeableDrawer, Drawer, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { SxProps, Theme } from '@mui/system';
 import MenuItems from './MenuItems';
 
 type MenuDrawerProps = {
@@ -10,16 +9,16 @@ type MenuDrawerProps = {
   onOpen: () => void,
 };
 
+const sxProp: SxProps<Theme> = {
+  overflowX: 'hidden',
+  overflow: 'overlay',
+  marginTop: '48px',
+  height: 'calc(100% - 48px)',
+};
+
 const MenuDrawer = ({ open, onClose, onOpen }: MenuDrawerProps) => {
   const theme = useTheme();
   const isExtraSmallWidth = useMediaQuery(theme.breakpoints.down('sm'));
-
-  const drawerChildren = (
-    <>
-      <Toolbar variant="dense" />
-      <MenuItems />
-    </>
-  );
 
   return (
     <>
@@ -29,18 +28,18 @@ const MenuDrawer = ({ open, onClose, onOpen }: MenuDrawerProps) => {
           open={open}
           onClose={onClose}
           onOpen={onOpen}
-          PaperProps={{ sx: { overflowX: 'hidden', overflow: 'overlay' } }}
+          PaperProps={{ sx: sxProp }}
         >
-          {drawerChildren}
+          <MenuItems />
         </SwipeableDrawer>
       ) : (
         <Drawer
           anchor="left"
           open={open}
           variant="persistent"
-          PaperProps={{ sx: { overflowX: 'hidden', overflow: 'overlay' } }}
+          PaperProps={{ sx: sxProp }}
         >
-          {drawerChildren}
+          <MenuItems />
         </Drawer>
       )}
     </>
