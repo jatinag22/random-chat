8a54eb647c80941a238a1a1cf550f523935c8ae2
Jatin Agrawal <agrawaljatin22@gmail.com>
2021-11-29 21:05:22 +0530

    improv: add tooltips in drawer items

--- packages/frontend/src/components/NavBar/DarkModeListItem.tsx
+++ packages/frontend/src/components/NavBar/DarkModeListItem.tsx
@@ -1,5 +1,5 @@
 import {
-  Checkbox, ListItem, ListItemIcon, ListItemText,
+  Checkbox, ListItem, ListItemIcon, ListItemText, Tooltip, useMediaQuery, useTheme,
 } from '@mui/material';
 import { DarkMode as DarkModeIcon, LightMode as LightModeIcon } from '@mui/icons-material';
 import { useCallback } from 'react';
@@ -7,26 +7,42 @@ import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { toggleTheme } from '../../redux/reducers/actions';
 
 const DarkModeListItem = () => {
-  const { theme } = useAppSelector((state) => state.app);
+  const { theme, isMenuDrawerOpen } = useAppSelector((state) => state.app);
   const dispatch = useAppDispatch();
+  const muiTheme = useTheme();
+  const isExtraSmallWidth = useMediaQuery(muiTheme.breakpoints.down('sm'));
 
   const toggleDarkMode = useCallback(() => {
     dispatch(toggleTheme({}));
   }, []);
 
   return (
-    <ListItem button onClick={toggleDarkMode}>
-      <ListItemIcon>
-        <Checkbox
-          inputProps={{ 'aria-labelledby': 'toggle dark mode' }}
-          checked={theme === 'dark'}
-          icon={<LightModeIcon />}
-          checkedIcon={<DarkModeIcon />}
-          sx={{ padding: 0 }}
-        />
-      </ListItemIcon>
-      <ListItemText primary="Dark Mode" />
-    </ListItem>
+    <Tooltip
+      title={!isExtraSmallWidth && !isMenuDrawerOpen ? 'Dark Mode' : ''}
+      placement="right"
+      componentsProps={{
+        tooltip: {
+          sx: {
+            '&.MuiTooltip-tooltip.MuiTooltip-tooltipPlacementRight': {
+              margin: '0px',
+            },
+          },
+        },
+      }}
+    >
+      <ListItem button onClick={toggleDarkMode}>
+        <ListItemIcon>
+          <Checkbox
+            inputProps={{ 'aria-labelledby': 'toggle dark mode' }}
+            checked={theme === 'dark'}
+            icon={<LightModeIcon />}
+            checkedIcon={<DarkModeIcon />}
+            sx={{ padding: 0 }}
+          />
+        </ListItemIcon>
+        <ListItemText primary="Dark Mode" />
+      </ListItem>
+    </Tooltip>
   );
 };
 
